54ac1a987a5a5392cf7e4e943a0fd60a7fdf1042
erick14117 <40038099+erick14117@users.noreply.github.com>
2024-01-04 03:40:15 +0000

    comments updated

--- index.js
+++ index.js
@@ -2,21 +2,26 @@
 
 let number = [5,10,15,60,8] 
 let maxNum = 0 
-//Zero because you should start with zero. 
+//[1]
 
 let size = number.length
-//"lenght" is a javscript function that returns the number of elements in an array.
+//[2]
 
 for(i=0; i<size;i++){
   if(maxNum<number[i]){
     maxNum = number[i]
   }
   
-}//The "i" in the for starts in 0 because thats the first number/position in the array.
-// In the first "if" condition, the "i" inside [i] is the current position in the array.
+}//[3] 
 
 console.log(maxNum)
 
 let max = Math.max(5,10,15,60,8)
 
 console.log(max)
+
+//[1]Zero because you should start with zero. 
+
+//[2]"lenght" is a javscript function that returns the number of elements in an array.
+
+//[3] The "i" in the for starts in 0 because thats the first number/position in the array. In the first "if" condition, the "i" inside [i] is the current position in the array.
